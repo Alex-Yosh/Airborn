2ac945bd68207a63294709b5d46dfb6193db706a
Alex Yoshida <a3yoshid@uwaterloo.ca>
2024-12-13 21:45:07 -0500

    initial setup

---
+++ Airborn/Airborn/AirbornApp.swift
@@ -0,0 +1,18 @@
+//
+//  AirbornApp.swift
+//  Airborn
+//
+//  Created by Alex Yoshida on 2024-12-13.
+//
+
+import SwiftUI
+
+@main
+struct AirbornApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Airborn/Airborn/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Airborn
+//
+//  Created by Alex Yoshida on 2024-12-13.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#Preview {
+    ContentView()
+}
