2353212eb194bf76d551a61382cab231e927e222
Alex Yoshida <a3yoshid@uwaterloo.ca>
2024-12-20 11:31:36 -0500

    added homeview, top text and scale

--- Airborn/Airborn/Classes/Screens/MainTab/HomeScreen/HomeView.swift
+++ Airborn/Airborn/Classes/Screens/MainTab/HomeScreen/HomeView.swift
@@ -9,7 +9,19 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Home")
+        ZStack{
+            //top gradiant
+            LinearGradient(colors: [Constants.Colour.PrimaryBlue, .white], startPoint: .top, endPoint: .bottom).ignoresSafeArea()
+            
+            VStack(spacing: 32){
+                HomeTopTextView()
+                Spacer()
+                //TODO: remove constant and add real valaues
+                HomeScaleView(progressPercent: 0.875)
+                Spacer()
+            }
+            
+        }
     }
 }
 
---
+++ Airborn/Airborn/Classes/Screens/MainTab/HomeScreen/SubViews/HomeTopTextView.swift
@@ -0,0 +1,42 @@
+//
+//  HomeTopTextView.swift
+//  Airborn
+//
+//  Created by Alex Yoshida on 2024-12-20.
+//
+
+import SwiftUI
+
+struct HomeTopTextView: View {
+    var body: some View {
+        VStack(spacing: 12){
+            Text(greeting())
+            Text(Date.now, format: .dateTime.day().month(.wide).year())
+        }
+    }
+    
+    func greeting() ->String{
+        let date = NSDate()
+        let calender = NSCalendar.current
+        let hour = calender.component(.hour, from: date as Date)
+        let hourInt = Int(hour.description)!
+        
+        switch(hourInt)
+        {
+        case 0...6:
+            return "You should sleep"
+        case 7...11:
+            return "Good Morning"
+        case 12...16:
+            return "Good Afternoon"
+        case 17...20:
+            return "Good Evening"
+        default:
+            return "Good Night"
+        }
+    }
+}
+
+#Preview {
+    HomeTopTextView()
+}
