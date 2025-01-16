76a20457d4bbc51b101663efba2ebc0f0fb325b2
Alex Yoshida <a3yoshid@uwaterloo.ca>
2025-01-16 17:21:29 -0500

    no message

--- Airborn/Airborn/Classes/Models/SensorData.swift
+++ Airborn/Airborn/Classes/Models/SensorData.swift
@@ -7,23 +7,12 @@
 
 import Foundation
 
-struct SensorData: Hashable, Identifiable{
-    let id = UUID() // Always unique on map
-    
-    var name: String
-    var lastUpdated: Date
-    
-    //location
-    var lat: Double
-    var long: Double
-    
-    //maybe
-    var battery: Double
-    
-    //data
-    var temp: Double
-    var humidity: Double
-    var PM25: Double
-    var TVOC: Double
-    var CO2: Double
+struct SensorData: Codable {
+    let sensorId: UUID
+    let temperature: Double
+    let humidity: Double
+    let pm25: Double
+    let tvoc: Double
+    let co2: Double
+    let date: Date?
 }
