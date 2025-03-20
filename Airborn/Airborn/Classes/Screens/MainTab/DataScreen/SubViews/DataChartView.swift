a25f8e9ddb5154a4067638f5597117a53918a7e2
Alex Yoshida <a3yoshid@uwaterloo.ca>
2025-03-20 16:29:14 -0400

    no message

--- Airborn/Airborn/Classes/Screens/MainTab/DataScreen/SubViews/DataChartView.swift
+++ Airborn/Airborn/Classes/Screens/MainTab/DataScreen/SubViews/DataChartView.swift
@@ -15,7 +15,6 @@ struct DataChartView: View {
     @State var data: [Double] = Array(repeating: 0.0, count: 24)
     let sensorType: Constants.dataTypes
     
-    @State private var animatedData: [Double] = Array(repeating: 0.0, count: 24)
     @State private var showChart: Bool = false
     
     // MARK: - Generate Time Ranges
@@ -42,10 +41,14 @@ struct DataChartView: View {
         }
     }
     
+    private var prefix: Int {
+        return dataManager.filterType == .lastDay ? 24 : 7
+    }
+    
     var body: some View {
         VStack {
             Chart {
-                let chartData = zip(dateRange, animatedData.prefix(dateRange.count))
+                let chartData = zip(dateRange, data.prefix(prefix))
                     .map { ($0, $1) } // Precompute
                 
                 ForEach(chartData, id: \.0) { (time, value) in
@@ -59,7 +62,8 @@ struct DataChartView: View {
                         x: .value("Time", time),
                         y: .value(sensorType.rawValue, value)
                     )
-                    .foregroundStyle(colorCode(sensorType: sensorType))                }
+                    .foregroundStyle(colorCode(sensorType: sensorType))
+                }
             }
             .chartYAxis {
                 AxisMarks(position: .trailing)
@@ -105,17 +109,16 @@ struct DataChartView: View {
             }
             .frame(height: 300)
             .opacity(showChart ? 1 : 0) // Fade-in effect
-            .scaleEffect(showChart ? 1 : 0.9, anchor: .center) // Scale-up effect
-            .animation(.spring(response: 1.2, dampingFraction: 0.8, blendDuration: 0.2), value: showChart) // Bounce effect
+            .animation(.easeIn(duration: 0.5), value: showChart) // Simple animation
             
         }
         .onAppear {
-            fetchData()
+            if data.isEmpty || !showChart {
+                fetchData()
+            }
         }
         .onChange(of: dataManager.filterType) { _, _ in
             showChart = false
-            data = Array(repeating: 0.0, count: 24)
-            animatedData = Array(repeating: 0.0, count: 24)
             fetchData()
         }
     }
@@ -124,27 +127,7 @@ struct DataChartView: View {
         dataManager.getUserAverages(type: sensorType) { result in
             DispatchQueue.main.async {
                 self.data = result
-                animateChart()
-            }
-        }
-    }
-    
-    // MARK: - Animate Data Points & Line Together
-    private func animateChart() {
-        if !showChart {
-            animatedData = [] // Reset before animation
-            DispatchQueue.main.asyncAfter(deadline: .now() ) {
-                withAnimation {
-                    showChart = true
-                }
-            }
-            
-            for (index, value) in data.enumerated() {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5 + Double(index) * 0.25) {
-                    withAnimation {
-                        animatedData.append(value) // Append values gradually
-                    }
-                }
+                showChart = true
             }
         }
     }
